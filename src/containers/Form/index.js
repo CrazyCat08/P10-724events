9c9aefa85ce457acc73e7e779139531b17129780
Anne Nawrocki <nawrocki.anne@gmail.com>
2024-03-07 00:54:37 +0100

    Message confirmation envoi formulaire de contact fonctionnel

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess(); // Message confirmation si call API a fonctionné
       } catch (err) {
         setSending(false);
         onError(err);
