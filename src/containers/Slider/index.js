d154642e57d7f67770d36a5233daa03945ae9a18
Anne Nawrocki <nawrocki.anne@gmail.com>
2024-03-09 19:03:05 +0100

    fix derniers warnings console

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,9 +7,9 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+  const byDateDesc =  data?.focus ? data.focus.sort((evtA, evtB) =>
   new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
-  );
+  ) : [];
   // new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   // Les évènements du slider doivent être classés du plus récent
   // au plus ancien
